6a3d3cd81edff3351ba6c065d7ac4427165c5201
edgar.klerks <edgar.klerks@sanoma.com>
2020-06-25 13:34:04 +0200

    added expect script for vpn

--- scripts/vpn.tcl
+++ scripts/vpn.tcl
@@ -5,17 +5,63 @@ set PASSWD [exec aws ssm get-parameter --name /personal/edgarklerks/ad-passwd --
 
 set TOKEN [lindex $argv 0] 
 
+puts "Login in with $TOKEN"
 
 
-spawn openconnect --user media\\E.Klerks  https://vpn-nl.sanoma.com/OTP
+spawn sudo openconnect https://vpn-nl.sanoma.com/OTP
 
+
+expect { 
+  "\[sudo\]" {
+      stty -echo
+      send_user -- "Password: "
+      expect_user -re "(.*)\n"
+      send_user "\n"
+      stty echo
+      set pass $expect_out(1,string)
+      send "$pass\r"
+    }
+    default {}
+    
+}
+
+expect "Username" {
+  send "media\\E.Klerks\r"
+}
 expect "Password" { 
   send "$PASSWD\r"
 }
+expect { 
+  "POST" {} 
+}
+
+expect {
+  "Login failed" {
+    puts "\n"
+    puts "Login failed for whatever reason"
+    exit 1 
+  }
+  "Enter"
+}
+
+
 
 expect "Response" {
   send "$TOKEN\r"
 }
-interact
+expect "POST"
+
+expect {
+  "Login failed" {
+    puts "\n"
+    puts "Login failed for whatever reason"
+    exit 1 
+
+  }
+  default { 
+    interact
+  }
+}
+
 
 
